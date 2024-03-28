642d73f7d933620fe976ddeb48232ede28f914e9
ayeshakhan-29 <aarkk.29@gmail.com>
2024-03-28 17:09:53 +0500

    services section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./Components/Sections/Hero";
 import Navbar from "./Components/Navigation/Navbar";
 import Footer from "./Components/Navigation/Footer";
+import Services from "./Components/Sections/Services";
 
 function App() {
   return (
     <div className="max-w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
+      <Services />
       <Footer />
     </div>
   );
--- src/Components/Sections/Hero.jsx
+++ src/Components/Sections/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="h-screen w-screen ">
-      <div className="flex flex-col justify-center text-white items-center h-full">
-        <h1 className="text-4xl font-bold">
-          Connecting Businesses to Digital Excelle
+      <div className="flex flex-col justify-center text-center md:mt-[-5rem] text-white items-center h-full mx-10 gap-5">
+        <h1 className="text-3xl md:text-6xl font-bold">
+          Channeling Imagination Beyond Limits
         </h1>
-        <p className="text-2xl">This is the Hero Section of the website</p>
+        <p className="text-xl">Crafting transformative solutions that seamlessly blend creativity, technology and strategy.</p>
       </div>
     </div>
   );
